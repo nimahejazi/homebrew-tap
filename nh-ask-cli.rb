46ace6f2b86781f76ffb6d700f1f93621caa3fc9
Nima Hejazi <nhejazi@gmail.com>
2026-08-10 08:29:43 -0700

    Updated to version v0.2.15

--- nh-ask-cli.rb
+++ nh-ask-cli.rb
@@ -3,8 +3,8 @@ class NhAskCli < Formula
 
   desc "AI CLI tool for natural language interaction with LLMs"
   homepage "https://github.com/nimahejazi/ask"
-  url "https://github.com/nimahejazi/ask/archive/refs/tags/v0.2.12.tar.gz"
-  sha256 "738cfc99eaadcbf7d2bb0b523903ac8cc0b4e776362a339669af7c9908e8e980"
+  url "https://github.com/nimahejazi/ask/archive/refs/tags/v0.2.15.tar.gz"
+  sha256 "194f46680a403d1cc7e6292a86a9217e4e3f89b7662c7910178e9cc77b2f0bd3"
   license "MIT"
 
   depends_on "python-setuptools" => :build
